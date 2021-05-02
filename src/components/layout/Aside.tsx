1326e495f57643f3e826442376c96227cfb829f8
Leandro Herenu <lherenu@coalfire.com>
2021-05-01 23:04:22 -0300

    dynamic menu

---
+++ src/components/layout/Aside.tsx
@@ -0,0 +1,35 @@
+import { useIntl } from "react-intl";
+import { ProSidebar, Menu, SidebarHeader, SidebarContent } from "react-pro-sidebar";
+
+import sidebarBg from "./../../assets/bg1.jpg";
+
+const Aside = ({ image, collapsed, children }) => {
+  const intl = useIntl();
+
+  return (
+    <ProSidebar image={image ? sidebarBg : null} collapsed={collapsed} breakPoint="md">
+      <SidebarHeader>
+        <div
+          style={{
+            padding: "24px",
+            textTransform: "uppercase",
+            fontWeight: "bold",
+            fontSize: 14,
+            letterSpacing: "1px",
+            overflow: "hidden",
+            textOverflow: "ellipsis",
+            whiteSpace: "nowrap"
+          }}
+        >
+          {intl.formatMessage({ id: "sidebarTitle" })}
+        </div>
+      </SidebarHeader>
+
+      <SidebarContent>
+        <Menu iconShape="circle">{children}</Menu>
+      </SidebarContent>
+    </ProSidebar>
+  );
+};
+
+export default Aside;
