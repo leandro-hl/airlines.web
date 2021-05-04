c5810fc6b30047ccf660280530dcf44d2f05ba8c
Leandro Herenu <lherenu@coalfire.com>
2021-05-04 17:48:56 -0300

    reusing rest api

---
+++ src/api/rest-api.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { buildUrl, buildHostUrl } from "./helpers";
+
+const getRestApi = (module: string) => {
+  const url = buildHostUrl(module);
+  return {
+    getAll: async () => {
+      return await axios.get(url);
+    },
+    post: async item => {
+      return await axios.post(url, item);
+    },
+    get: async (id: number) => {
+      return await axios.get(buildUrl(url, id));
+    },
+    put: async (id: number, item) => {
+      return await axios.put(buildUrl(url, id), item);
+    }
+  };
+};
+
+export { getRestApi };
