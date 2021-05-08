e9d162dcab0de14ce7e5a215c4db2108119ef46e
Leandro Herenu <lherenu@coalfire.com>
2021-05-08 18:53:30 -0300

    improvements on Select component

--- src/components/shared/Select.tsx
+++ src/components/shared/Select.tsx
@@ -1,22 +1,45 @@
+import { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
 
-const Select = ({ label, options, selected, onChange }) => {
-  const selectOptions = options.map(opt => {
+const generateOptions = options => {
+  return options?.map(opt => {
     return (
       <option value={opt.val} key={opt.val}>
         {opt.desc}
       </option>
     );
   });
+};
+
+const Select = ({ name, options, defaultValue, placeholder, onChange }) => {
+  const [selectOptions, setOptions] = useState([]);
+
+  useEffect(() => {
+    if (options instanceof Function) {
+      options().then(r => setOptions(generateOptions(r)));
+    } else {
+      setOptions(generateOptions(options));
+    }
+  }, []);
 
   return (
     <>
-      <Form.Label>{label}</Form.Label>
-      <Form.Control as="select" custom onChange={onChange} value={selected}>
+      <Form.Control
+        as="select"
+        custom
+        name={name}
+        onChange={onChange}
+        value={defaultValue}
+        placeholder={placeholder}
+        defaultValue={defaultValue}
+      >
+        <option disabled selected={!defaultValue}>
+          {placeholder}
+        </option>
         {selectOptions}
       </Form.Control>
     </>
   );
 };
 
-export default Select;
+export { Select };
